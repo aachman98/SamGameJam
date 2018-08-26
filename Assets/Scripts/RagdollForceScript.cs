5c0530f79a3df55da35264e4557a2a330ce46879
Punya Aachman <aachman98@github.com>
2018-08-26 22:26:49 +0530

    Update: Ragdoll initial force script added

---
+++ Assets/Scripts/RagdollForceScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollForceScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(200, 50));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
