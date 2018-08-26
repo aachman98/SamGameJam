e3c911b94fc8649b5723c272c0713ae3707ded1d
Punya Aachman <aachman98@github.com>
2018-08-26 22:43:37 +0530

    Debug: Get click or tap position on screen

---
+++ Assets/Scripts/TapInputScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapInputScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse position: " + Input.mousePosition);
+        }
+        if (Input.touchCount > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch position: " + Input.touches[0].position);
+            }
+        }
+	}
+}
