64c3feae3e43473bf9ea13fb3713dfab9a4a8de0
Punya Aachman <aachman98@github.com>
2018-08-26 23:38:58 +0530

    Update: Trampoline spawn on click position

--- Assets/Scripts/TapInputScript.cs
+++ Assets/Scripts/TapInputScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TapInputScript : MonoBehaviour {
 
+    public GameObject trampoline;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,13 +16,20 @@ public class TapInputScript : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Mouse position: " + Input.mousePosition);
+            SpawnTrampoline(Input.mousePosition.x);
         }
         if (Input.touchCount > 0)
         {
             if (Input.touches[0].phase == TouchPhase.Began)
             {
                 Debug.Log("Touch position: " + Input.touches[0].position);
+                SpawnTrampoline(Input.touches[0].position.x);
             }
         }
 	}
+
+    void SpawnTrampoline(float xCoordinate)
+    {
+        Instantiate(trampoline).transform.position = new Vector3(Camera.main.ScreenToWorldPoint(new Vector3(xCoordinate, 0, 0)).x, -3.5f, 0);
+    }
 }
