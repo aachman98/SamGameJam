29eb935743db5c149b1b5615831ffb8c3dcc0813
Punya Aachman <aachman98@github.com>
2018-08-26 22:36:27 +0530

    Update: Camera movement script added

---
+++ Assets/CameraMovementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour {
+
+    public GameObject ragdoll;
+    private bool followRagdoll = true;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (followRagdoll)
+        {
+            if (ragdoll.transform.position.x > 0)
+                transform.position = new Vector3(ragdoll.transform.position.x, transform.position.y, transform.position.z);
+        }
+	}
+
+    public void StopCameraMovement()
+    {
+        followRagdoll = false;
+    }
+}
