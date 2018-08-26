30481d2fa2d5b4e4e547de2dc76d8793484b1755
Punya Aachman <aachman98@github.com>
2018-08-26 22:51:06 +0530

    Testing: Trampoline force on collision script

---
+++ Assets/TrampolineForceScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampolineForceScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.collider.GetComponent<Rigidbody2D>().AddForce(new Vector3(200, 2000, 0));
+    }
+}
