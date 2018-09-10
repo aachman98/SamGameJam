9e5b20ecf3beaf25b0b31efecd4a7cf780187386
Punya Aachman <aachman98@github.com>
2018-09-10 14:23:13 +0530

    Feature: play again button

---
+++ Assets/SceneManagerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerScript : MonoBehaviour {
+
+    public GameObject button, text;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void RestartLevel()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(0);
+        button.SetActive(false);
+        text.SetActive(true);
+    }
+}
